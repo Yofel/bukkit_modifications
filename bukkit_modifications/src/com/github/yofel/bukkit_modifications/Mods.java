ad05c6c521e25ce5fe8927f539614bd7a63feeaf
Philip Muskovac <yofel@gmx.net>
2012-08-02 21:37:07 +0200

    initial import

---
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/Mods.java
@@ -0,0 +1,19 @@
+package com.github.yofel.bukkit_modifications;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Mods extends JavaPlugin
+{
+  @Override
+  public void onEnable()
+  {
+    getLogger().info("bukkit_mods 0.1 enabled");
+  }
+  
+  @Override
+  public void onDisable()
+  {
+    getLogger().info("bukkit_mods 0.1 disabled");
+  }
+
+}
