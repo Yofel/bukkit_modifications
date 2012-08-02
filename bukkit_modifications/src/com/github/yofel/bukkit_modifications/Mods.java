b754892a92a61aa90b01ffd5d83570501778fb2f
Philip Muskovac <yofel@gmx.net>
2012-08-02 22:31:49 +0200

    add debug information

--- bukkit_modifications/src/com/github/yofel/bukkit_modifications/Mods.java
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/Mods.java
@@ -5,14 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Mods extends JavaPlugin {
     @Override
     public void onEnable() {
-	getLogger().info("bukkit_mods 0.1 enabled");
-	getServer().getPluginManager().registerEvents(new VehicleListener(),
-		this);
+        getLogger().info("bukkit_mods 0.1 enabled");
+        getServer().getPluginManager().registerEvents(new VehicleListener(),
+                this);
     }
 
     @Override
     public void onDisable() {
-	getLogger().info("bukkit_mods 0.1 disabled");
+        getLogger().info("bukkit_mods 0.1 disabled");
     }
 
 }
--- bukkit_modifications/src/com/github/yofel/bukkit_modifications/VehicleListener.java
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/VehicleListener.java
@@ -1,5 +1,7 @@
 package com.github.yofel.bukkit_modifications;
 
+import java.util.logging.Logger;
+
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
@@ -7,12 +9,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleCreateEvent;
 
 public class VehicleListener implements Listener {
+    
+    Logger log = Logger.getLogger("Minecraft");
+    
     @EventHandler
     public void onVehicleCreation(VehicleCreateEvent e) {
-	if (e.getVehicle().getType() == EntityType.MINECART) {
-	    Minecart m = (Minecart) e.getVehicle();
-	    m.setMaxSpeed(0.8D);
-	}
+        if (e.getVehicle().getType() == EntityType.MINECART) {
+            Minecart m = (Minecart) e.getVehicle();
+            m.setMaxSpeed(0.8D);
+            log.info("Mincart placed");
+        }
 
     }
 
