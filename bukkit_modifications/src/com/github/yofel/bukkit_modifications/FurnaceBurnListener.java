ac47b0b1f0719d30f1f52ebc85a557fe06ad4e0a
Philip Muskovac <yofel@gmx.net>
2012-08-03 19:43:08 +0200

    as 1.3 returns the bucket throw the lava code out

--- bukkit_modifications/src/com/github/yofel/bukkit_modifications/FurnaceBurnListener.java
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/FurnaceBurnListener.java
@@ -26,54 +26,48 @@ public class FurnaceBurnListener implements Listener {
             
             Furnace furnaceBlock = (Furnace) event.getBlock().getState();
 
-            // Copyright ArmEagle on github
+            /*
             if (event.getFuel().getType() == Material.LAVA_BUCKET) {
-                    org.bukkit.material.Furnace furnace = (org.bukkit.material.Furnace) furnaceBlock
-                            .getData();
-    
-                    Location front = furnaceBlock.getBlock()
-                            .getRelative(furnace.getFacing()).getLocation();
-                    furnaceBlock.getWorld().dropItem(front,
-                            new ItemStack(Material.BUCKET));
             }
+            */
             
             // add custom fuels
-            else if (event.getFuel().getType() == Material.SEEDS) {
+            if (event.getFuel().getType() == Material.SEEDS) {
                 // FIXME: the burndown is buggy if the burn time is < 200
                 furnaceBlock.setBurnTime((short) 25);
             }
             
-            else if (event.getFuel().getType() == Material.SULPHUR) {
+            if (event.getFuel().getType() == Material.SULPHUR) {
                 // GUNPOWDER
                 furnaceBlock.setBurnTime((short) 200);
             }
             
-            else if (event.getFuel().getType() == Material.WHEAT) {
+            if (event.getFuel().getType() == Material.WHEAT) {
                 furnaceBlock.setBurnTime((short) 100);
             }
             
-            else if (event.getFuel().getType() == Material.SUGAR_CANE) {
+            if (event.getFuel().getType() == Material.SUGAR_CANE) {
                 furnaceBlock.setBurnTime((short) 100);
             }
             
-            else if (event.getFuel().getType() == Material.WOODEN_DOOR) {
+            if (event.getFuel().getType() == Material.WOODEN_DOOR) {
                 // make it equal 4 blocks of wood
                 furnaceBlock.setBurnTime((short) 1200);
             }
             
-            else if (event.getFuel().getType() == Material.VINE) {
+            if (event.getFuel().getType() == Material.VINE) {
                 furnaceBlock.setBurnTime((short) 100);
             }
             
-            else if (event.getFuel().getType() == Material.LEAVES) {
+            if (event.getFuel().getType() == Material.LEAVES) {
                 furnaceBlock.setBurnTime((short) 50);
             }
             
-            else if (event.getFuel().getType() == Material.WOOL) {
+            if (event.getFuel().getType() == Material.WOOL) {
                 furnaceBlock.setBurnTime((short) 100);
             }
             
-            else if (event.getFuel().getType() == Material.FIREBALL) {
+            if (event.getFuel().getType() == Material.FIREBALL) {
                 // equals one piece of coal
                 // that's too much, but 800 is too low and everything in between
                 // has 64 % x != 0
