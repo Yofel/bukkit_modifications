0749f5129449ca003d7be6ee33e7df7d883af7bf
Philip Muskovac <yofel@gmx.net>
2012-08-03 16:08:50 +0200

    refactor a bit and add gunpowder to burnables

--- bukkit_modifications/src/com/github/yofel/bukkit_modifications/FurnaceBurnListener.java
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/FurnaceBurnListener.java
@@ -17,26 +17,40 @@ public class FurnaceBurnListener implements Listener {
     @EventHandler
     public void onFurnaceBurn(FurnaceBurnEvent event) {
         
+        /*
         log.info("Furnace activated with " + event.getFuel().toString());
-
-        // Copyright ArmEagle on github
-        if (event.getFuel().getType() == Material.LAVA_BUCKET) {
-            try {
-                Furnace furnaceBlock = (Furnace) event.getBlock().getState();
-                org.bukkit.material.Furnace furnace = (org.bukkit.material.Furnace) furnaceBlock
-                        .getData();
-
-                Location front = furnaceBlock.getBlock()
-                        .getRelative(furnace.getFacing()).getLocation();
-                furnaceBlock.getWorld().dropItem(front,
-                        new ItemStack(Material.BUCKET));
-            } catch (Exception e) {
-                // ignore, furnace probably was no furnace anymore.
-            }
-        }
         
-        // add custom fuels
-        else if (event.getFuel().getType() == Material.SEEDS) {
+        log.info("Original Burn time: " + event.getBurnTime());
+        */
+        try {
+            
+            Furnace furnaceBlock = (Furnace) event.getBlock().getState();
+
+            // Copyright ArmEagle on github
+            if (event.getFuel().getType() == Material.LAVA_BUCKET) {
+                    org.bukkit.material.Furnace furnace = (org.bukkit.material.Furnace) furnaceBlock
+                            .getData();
+    
+                    Location front = furnaceBlock.getBlock()
+                            .getRelative(furnace.getFacing()).getLocation();
+                    furnaceBlock.getWorld().dropItem(front,
+                            new ItemStack(Material.BUCKET));
+            }
+            
+            // add custom fuels
+            else if (event.getFuel().getType() == Material.SEEDS) {
+                furnaceBlock.setBurnTime((short) 50);
+                // FIXME: the burndown is buggy if the burn time is < 200
+            }
+            
+            else if (event.getFuel().getType() == Material.SULPHUR) {
+                furnaceBlock.setBurnTime((short) 200);
+                // GUNPOWDER
+            
+            }
+        
+        } catch (Exception e) {
+            // ignore, furnace probably was no furnace anymore.
         }
     }
 
