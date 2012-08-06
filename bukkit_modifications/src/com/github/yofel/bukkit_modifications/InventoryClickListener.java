2e9e72ac5a4b1840942f0a50eb0c6cdff4654604
Philip Muskovac <yofel@gmx.net>
2012-08-06 21:50:03 +0200

    failed attempt to get the shift click working

--- bukkit_modifications/src/com/github/yofel/bukkit_modifications/InventoryClickListener.java
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/InventoryClickListener.java
@@ -7,56 +7,113 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryClickListener implements Listener {
-    
+
     Logger log = Bukkit.getLogger();
-    
+
     @EventHandler
     public boolean onInventoryClick(InventoryClickEvent e) {
-        
+
         // FIXME: so far it's only handling direct clicks
-        if (! e.isShiftClick()) {
-            
-            if (e.getCursor().getType() != Material.AIR
-                    && e.getCurrentItem().getType() != Material.AIR
-                    && e.getCurrentItem().getData().equals(e.getCurrentItem().getData())
-                    && e.getCurrentItem().getDurability() == e.getCursor().getDurability()) {
-                
-                if (e.getCurrentItem().getType() == Material.POTION
-                        || e.getCurrentItem().getType() == Material.MUSHROOM_SOUP) {
-                    
-                    int newAmountInInventory = e.getCurrentItem().getAmount() +
-                            e.getCursor().getAmount();
-                    if (newAmountInInventory > 64) newAmountInInventory = 64;
-                    
-                    int newAmountInHand = e.getCursor().getAmount() -
-                           (newAmountInInventory - e.getCurrentItem().getAmount());
-                    
-                    if (newAmountInInventory < 0) log.warning("amount < 0, this shouldn't happen!");
-                    
-                    e.setCurrentItem(new ItemStack(e.getCurrentItem().getType(),
-                            newAmountInInventory,
+        if (e.getCurrentItem().getType() == Material.POTION
+                || e.getCurrentItem().getType() == Material.MUSHROOM_SOUP) {
+            if (!e.isShiftClick()) {
+
+                if (e.getCursor().getType() != Material.AIR
+                        && e.getCurrentItem().getType() != Material.AIR
+                        && e.getCurrentItem().getData()
+                                .equals(e.getCurrentItem().getData())
+                        && e.getCurrentItem().getDurability() == e.getCursor()
+                                .getDurability()) {
+
+                    int newAmountInInventory = e.getCurrentItem().getAmount()
+                            + e.getCursor().getAmount();
+                    if (newAmountInInventory > 64)
+                        newAmountInInventory = 64;
+
+                    int newAmountInHand = e.getCursor().getAmount()
+                            - (newAmountInInventory - e.getCurrentItem()
+                                    .getAmount());
+
+                    if (newAmountInInventory < 0)
+                        log.warning("amount < 0, this shouldn't happen!");
+
+                    e.setCurrentItem(new ItemStack(
+                            e.getCurrentItem().getType(), newAmountInInventory,
                             e.getCurrentItem().getDurability()));
                     e.setCursor(new ItemStack(e.getCursor().getType(),
-                            newAmountInHand,
-                            e.getCursor().getDurability()));
-                    
+                            newAmountInHand, e.getCursor().getDurability()));
+
                     e.setCancelled(true);
-                    
+
                     return true;
                 }
+            } else {
+                // FIXME: BROKEN
+                /*
+                // only do this where it makes sense
+                switch (e.getInventory().getType()) {
+                case CHEST:
+                case DISPENSER:
+                case PLAYER:
+                case ENDER_CHEST:
+                case BREWING:
+                case MERCHANT:
+                case CRAFTING:
+                    ItemStack is = e.getCurrentItem();
+                    Inventory inv = e.getInventory();
+                    log.info("itemstack: " + is);
+
+                    for (int i = 0; i < inv.getSize(); i++) {
+                        ItemStack item = inv.getItem(i);
+                        log.info("Slot " + i + " has: " + item);
+                        
+                        if (item != null && item.getType() == is.getType()
+                                && item.getDurability() == is.getDurability()
+                                && item.getAmount() < 64) {
+                            
+                            int remainingAmount = 64 - item.getAmount();
+                            log.info("current amount:" + is.getAmount());
+                            log.info("remaining amount: " + remainingAmount);
+                            
+                            if (remainingAmount >= is.getAmount()) {
+                                inv.setItem(i, new ItemStack(is.getType(),
+                                        item.getAmount() + is.getAmount(),
+                                        is.getDurability()));
+                                e.setCurrentItem(null);
+                                return true;
+                            } 
+                            inv.setItem(i, new ItemStack(is.getType(),
+                                    64,
+                                    is.getDurability()));
+                            is = new ItemStack(is.getType(),
+                                    is.getAmount() - remainingAmount,
+                                    is.getDurability());
+
+                        }
+
+                    }
+                    
+                    // as all partly filled slots are now full try to take a new one
+                    log.info("first empty: " + inv.firstEmpty());
+                    if (inv.firstEmpty() >= 0)
+                    {
+                        inv.setItem(inv.firstEmpty(), is);
+                        is = null;
+                    }
+                    
+                    // all full so leave the rest where it is
+                    e.setCurrentItem(is);
+                    return true;
+                }
+                */
+
             }
         }
-        else {
-            // cursor is AIR
-//            log.info("Current data: " + e.getCurrentItem().getData());
-//            log.info("Current durability: " + e.getCurrentItem().getDurability());
-            
-            // TODO: look through every slot and try to fill them up
-        }
-        
+
         return false;
     }
 
