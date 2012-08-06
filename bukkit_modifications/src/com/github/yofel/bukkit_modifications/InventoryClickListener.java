99b89fc41e956b599f1dae5a70d518be6ad97fc9
Philip Muskovac <yofel@gmx.net>
2012-08-06 22:26:34 +0200

    also comment the import out to fix a warning

--- bukkit_modifications/src/com/github/yofel/bukkit_modifications/InventoryClickListener.java
+++ bukkit_modifications/src/com/github/yofel/bukkit_modifications/InventoryClickListener.java
@@ -7,7 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
+//import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryClickListener implements Listener {
